46fb3d22fe4b0408d71cd595cb5b76b033d5c6d4
GemStore <panchalchirag0210@gmail.com>
2024-05-14 18:48:29 +0530

    Changes

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -33,46 +33,71 @@ class _MyHomePageState extends State<MyHomePage> {
                 child: Container(
                   child: Column(
                     children: [
-                      DropdownButton<int>(
-                        dropdownColor: Constants.homeBackground,
-                        value: totalGames,
-                        items: [1, 3, 5, 7].map((int value) {
-                          return DropdownMenuItem<int>(
-                            value: value,
-                            child: Text('$value Games', style: Constants.myNewFontWhite),
-                          );
-                        }).toList(),
-                        onChanged: (value) {
-                          setState(() {
-                            totalGames = value!;
-                            currentGame = 1;
-                            ohScore = 0;
-                            xhScore = 0;
-                            _clearBoard();
-                          });
-                        },
+                      Padding(
+                        padding: const EdgeInsets.only(top: 30.0,left: 30,right: 30),
+                        child: DropdownButtonFormField<int>(
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                borderSide: BorderSide.none
+                              )),
+                          dropdownColor: Constants.homeBackground,
+                          value: totalGames,
+                          items: [1, 3, 5, 7].map((int value) {
+                            return DropdownMenuItem<int>(
+                              value: value,
+                              child: Text('$value Games',
+                                  style: Constants.myNewFontWhite),
+                            );
+                          }).toList(),
+                          onChanged: (value) {
+                            setState(() {
+                              totalGames = value!;
+                              currentGame = 1;
+                              ohScore = 0;
+                              xhScore = 0;
+                              _clearBoard();
+                            });
+                          },
+                        ),
                       ),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           Padding(
-                            padding: const EdgeInsets.only(top: 30.0, left: 25.0, right: 25.0, bottom: 30.0),
+                            padding: const EdgeInsets.only(
+                                top: 30.0,
+                                left: 25.0,
+                                right: 25.0,
+                                bottom: 30.0),
                             child: Column(
                               children: [
-                                Text('Player X', style: Constants.myNewFontWhite),
-                                const SizedBox(height: 20,),
-                                Text(xhScore.toString(), style: Constants.myNewFontWhite),
+                                Text('Player X',
+                                    style: Constants.myNewFontWhite),
+                                const SizedBox(
+                                  height: 20,
+                                ),
+                                Text(xhScore.toString(),
+                                    style: Constants.myNewFontWhite),
                               ],
                             ),
                           ),
                           const SizedBox(width: 20),
                           Padding(
-                            padding: const EdgeInsets.only(top: 30.0, left: 25.0, right: 25.0, bottom: 30.0),
+                            padding: const EdgeInsets.only(
+                                top: 30.0,
+                                left: 25.0,
+                                right: 25.0,
+                                bottom: 30.0),
                             child: Column(
                               children: [
-                                Text('Player O', style: Constants.myNewFontWhite),
-                                const SizedBox(height: 20,),
-                                Text(ohScore.toString(), style: Constants.myNewFontWhite),
+                                Text('Player O',
+                                    style: Constants.myNewFontWhite),
+                                const SizedBox(
+                                  height: 20,
+                                ),
+                                Text(ohScore.toString(),
+                                    style: Constants.myNewFontWhite),
                               ],
                             ),
                           ),
@@ -86,14 +111,16 @@ class _MyHomePageState extends State<MyHomePage> {
                 flex: 3,
                 child: GridView.builder(
                   itemCount: 9,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3),
                   itemBuilder: (BuildContext context, int index) {
                     return GestureDetector(
                       onTap: () {
                         _tapped(index);
                       },
                       child: Container(
-                        decoration: BoxDecoration(border: Border.all(color: Constants.border)),
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Constants.border)),
                         child: Center(
                           child: Text(
                             displayExOh[index],
@@ -103,30 +130,6 @@ class _MyHomePageState extends State<MyHomePage> {
                       ),
                     );
                   },
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(30),
-                child: GestureDetector(
-                  onTap: (){
-                    setState(() {
-                      ohScore = 0;
-                      xhScore = 0;
-                      currentGame = 1;
-                      _clearBoard();
-                    });
-                  },
-                  child: Padding(
-                    padding: EdgeInsets.only(left: 40, right: 40, bottom: 60),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: Container(
-                        padding: EdgeInsets.all(30),
-                        color: Colors.white,
-                        child: Text('RESTART WAR', style: Constants.myNewFont),
-                      ),
-                    ),
-                  ),
                 ),
               ),
             ],
@@ -154,49 +157,65 @@ class _MyHomePageState extends State<MyHomePage> {
   void _checkWinner() {
     bool winnerFound = false;
     // Check 1st row
-    if (displayExOh[0] == displayExOh[1] && displayExOh[0] == displayExOh[2] && displayExOh[0] != '') {
+    if (displayExOh[0] == displayExOh[1] &&
+        displayExOh[0] == displayExOh[2] &&
+        displayExOh[0] != '') {
       winnerFound = true;
       _showWinDialog(displayExOh[0]);
     }
 
     // Check 1st column
-    if (displayExOh[0] == displayExOh[3] && displayExOh[0] == displayExOh[6] && displayExOh[0] != '') {
+    if (displayExOh[0] == displayExOh[3] &&
+        displayExOh[0] == displayExOh[6] &&
+        displayExOh[0] != '') {
       winnerFound = true;
       _showWinDialog(displayExOh[0]);
     }
 
     // Check diagonal
-    if (displayExOh[0] == displayExOh[4] && displayExOh[0] == displayExOh[8] && displayExOh[0] != '') {
+    if (displayExOh[0] == displayExOh[4] &&
+        displayExOh[0] == displayExOh[8] &&
+        displayExOh[0] != '') {
       winnerFound = true;
       _showWinDialog(displayExOh[0]);
     }
 
     // Check 2nd column
-    if (displayExOh[1] == displayExOh[4] && displayExOh[1] == displayExOh[7] && displayExOh[1] != '') {
+    if (displayExOh[1] == displayExOh[4] &&
+        displayExOh[1] == displayExOh[7] &&
+        displayExOh[1] != '') {
       winnerFound = true;
       _showWinDialog(displayExOh[1]);
     }
 
     // Check 3rd column
-    if (displayExOh[2] == displayExOh[5] && displayExOh[2] == displayExOh[8] && displayExOh[2] != '') {
+    if (displayExOh[2] == displayExOh[5] &&
+        displayExOh[2] == displayExOh[8] &&
+        displayExOh[2] != '') {
       winnerFound = true;
       _showWinDialog(displayExOh[2]);
     }
 
     // Check 2nd row
-    if (displayExOh[3] == displayExOh[4] && displayExOh[3] == displayExOh[5] && displayExOh[3] != '') {
+    if (displayExOh[3] == displayExOh[4] &&
+        displayExOh[3] == displayExOh[5] &&
+        displayExOh[3] != '') {
       winnerFound = true;
       _showWinDialog(displayExOh[3]);
     }
 
     // Check 3rd row
-    if (displayExOh[6] == displayExOh[7] && displayExOh[6] == displayExOh[8] && displayExOh[6] != '') {
+    if (displayExOh[6] == displayExOh[7] &&
+        displayExOh[6] == displayExOh[8] &&
+        displayExOh[6] != '') {
       winnerFound = true;
       _showWinDialog(displayExOh[6]);
     }
 
     // Check reverse diagonal
-    if (displayExOh[2] == displayExOh[4] && displayExOh[2] == displayExOh[6] && displayExOh[2] != '') {
+    if (displayExOh[2] == displayExOh[4] &&
+        displayExOh[2] == displayExOh[6] &&
+        displayExOh[2] != '') {
       winnerFound = true;
       _showWinDialog(displayExOh[2]);
     }
@@ -213,14 +232,20 @@ class _MyHomePageState extends State<MyHomePage> {
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text('Draw'),
+          title: Text(
+            'Draw',
+            style: TextStyle(fontSize: 30),
+          ),
           actions: [
             TextButton(
               onPressed: () {
                 _clearBoard();
                 Navigator.of(context).pop();
               },
-              child: const Text('Play Again!'),
+              child: const Text(
+                'Play Again!',
+                style: TextStyle(fontSize: 20),
+              ),
             ),
           ],
         );
@@ -229,37 +254,40 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void _showWinDialog(String winner) {
-    showDialog(
-      barrierDismissible: false,
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Winner is: $winner'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                if (currentGame < totalGames) {
-                  _clearBoard();
-                  Navigator.of(context).pop();
-                  setState(() {
-                    currentGame += 1;
-                  });
-                } else {
-                  Navigator.of(context).pop();
-                  _showOverallWinner();
-                }
-              },
-              child: const Text('Next Game!'),
-            ),
-          ],
-        );
-      },
-    );
-
     if (winner == 'O') {
       ohScore += 1;
     } else if (winner == 'X') {
       xhScore += 1;
+    }
+
+    if (_checkEarlyWin()) {
+      _showOverallWinner();
+    } else {
+      showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Winner is: $winner'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  if (currentGame < totalGames) {
+                    _clearBoard();
+                    setState(() {
+                      currentGame += 1;
+                    });
+                  } else {
+                    _showOverallWinner();
+                  }
+                  Navigator.of(context).pop();
+                },
+                child: const Text('Next Game!'),
+              ),
+            ],
+          );
+        },
+      );
     }
   }
 
@@ -278,7 +306,10 @@ class _MyHomePageState extends State<MyHomePage> {
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text('Overall Winner is: $overallWinner'),
+          title: Text(
+            'Overall Winner is: $overallWinner',
+            style: TextStyle(fontSize: 30),
+          ),
           actions: [
             TextButton(
               onPressed: () {
@@ -290,12 +321,20 @@ class _MyHomePageState extends State<MyHomePage> {
                 });
                 Navigator.of(context).pop();
               },
-              child: const Text('Play Again!'),
+              child: const Text(
+                'Play Again!',
+                style: TextStyle(fontSize: 20),
+              ),
             ),
           ],
         );
       },
     );
+  }
+
+  bool _checkEarlyWin() {
+    int gamesToWin = (totalGames / 2).ceil();
+    return (ohScore >= gamesToWin || xhScore >= gamesToWin);
   }
 
   void _clearBoard() {
